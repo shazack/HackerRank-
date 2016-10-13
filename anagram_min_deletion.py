48baa5a4660aa6e83ed9d9d53a58063a4198e2e3
Nisha Shahul Hameed <nisharazack18@gmail.com>
2016-10-13 12:07:09 -0700

    Add files via upload

---
+++ anagram_min_deletion.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+def number_needed(a, b):
+    count_a = Counter(a)
+    count_b = Counter(b)
+    count_a.subtract(count_b)
+    final_sum = sum(abs(i) for i in count_a.values())
+    return final_sum
+
+
+   
+    
+   
+
+
+a = raw_input().strip()
+b = raw_input().strip()
+
+print number_needed(a, b)
